f6191f3818242f51605a9b9c7127946a802a005d
Alan (Quey-Liang) Kao <alankao@andestech.com>
2021-01-16 20:51:53 +0800

    Replace dokuwiki by hugo as the blog engine

--- entry.sh
+++ entry.sh
@@ -5,7 +5,7 @@ tmux new-session -d -s $NAME
 
 tmux send-keys -t "=$NAME:=0" \
 "echo -e 'To initialize, visit the following \n \
-Blog: http://localhost:8327/blog/install.php \n \
+Blog: http://localhost:8327/blog \n \
 Wiki: http://localhost:8327/note/install.php \n \
 Git: http://localhost:8327/install'" \
 	Enter
@@ -19,6 +19,11 @@ tmux send-keys -t "=$NAME:=server" \
 	Enter
 tmux send-keys -t "=$NAME:=server" \
 	"nginx" \
+	Enter
+
+tmux new-window -d -t "=$NAME" -n 'hugo'
+tmux send-keys -t "=$NAME:=hugo" \
+	"cd /usr/share/webapps/dokuwiki/blog && hugo && hugo serve" \
 	Enter
 
 tmux attach-session -t "=$NAME"
